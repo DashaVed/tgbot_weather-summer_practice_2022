f95f57017cb9044e1d7794808fa394b6ab5bf237
Dasha <dasha.ved93@gmail.com>
2022-07-11 18:35:44 +0300

    add docstrings

--- tg-bot-weather.py
+++ tg-bot-weather.py
@@ -2,7 +2,6 @@ import json
 import os
 import requests
 
-
 FUNC_RESPONSE = {'statusCode': 200, 'body': ''}
 TELEGRAM_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}'
@@ -13,6 +12,7 @@ DD_SEC_KEY = os.environ.get('DD_SEC_KEY')
 
 
 def send_message(text, message):
+    """Отправка сообщения пользователю Telegram."""
     message_id = message['message_id']
     chat_id = message['chat']['id']
     reply_message = {'chat_id': chat_id,
@@ -22,57 +22,66 @@ def send_message(text, message):
 
 
 def handler(event, context):
+    """Обработчик облачной функции."""
     if TELEGRAM_BOT_TOKEN is None:
         return FUNC_RESPONSE
     if OW_API_KEY is None:
         return FUNC_RESPONSE
+
     update = json.loads(event['body'])
+
     if 'message' not in update:
         return FUNC_RESPONSE
     message_in = update['message']
+
     if 'location' in message_in:
         echo_text = get_weather_info(message_in['location']["latitude"], message_in['location']["longitude"])
-        send_message(echo_text, message_in)
     elif 'text' in message_in:
+        if message_in['text'] == '/start':
+            echo_text = 'Введите адрес текстовым сообщением, голосовым сообщением или отправьте свою геолокацию,' \
+                        ' чтобы получить информацию о погоде'
+            send_message(echo_text, message_in)
+            return FUNC_RESPONSE
         if DD_API_KEY is None:
             return FUNC_RESPONSE
         if DD_SEC_KEY is None:
             return FUNC_RESPONSE
         address = message_in["text"].encode('utf-8').decode('utf-8')
-        lat, lon = get_coords_from_address(address)
-        if lat and lon:
-            echo_text = get_weather_info(lat, lon)
-            send_message(echo_text, message_in)
-        else:
-            send_message(f'Недостаточно информации для адреса {address}.', message_in)
+        echo_text = get_echo_text(address)
     elif 'voice' in message_in:
         # ищем путь аудио файла
         r_file = requests.get(url=f'{TELEGRAM_API_URL}/getFile', params={'file_id': message_in['voice']['file_id']})
-        if r_file.ok:
-            file_path = r_file.json()['result']['file_path']
+        file_path = r_file.json()['result']['file_path']
         # скачиваем файл из телеграмма
         r_file_download = requests.get(url=f'https://api.telegram.org/file/bot{TELEGRAM_BOT_TOKEN}/{file_path}')
-        if r_file_download.ok:
-            audio_file = r_file_download.content
+        audio_file = r_file_download.content
         # отправляем запрос YC SpeechKit
-            r_stt = requests.post(url='https://stt.api.cloud.yandex.net/speech/v1/stt:recognize',
-                                  headers={"Authorization": f"Api-Key {YS_API_KEY}"},
-                                  data=audio_file)
-            if r_stt.ok:
-                address = r_stt.json()['result']
-                lat, lon = get_coords_from_address(address)
-                if lat and lon:
-                    echo_text = get_weather_info(lat, lon)
-                    send_message(echo_text, message_in)
-                else:
-                    send_message(f'Недостаточно информации для адреса {address}.', message_in)
+        r_stt = requests.post(url='https://stt.api.cloud.yandex.net/speech/v1/stt:recognize',
+                              headers={"Authorization": f"Api-Key {YS_API_KEY}"},
+                              data=audio_file)
+        if r_stt.ok:
+            address = r_stt.json()['result']
+            echo_text = get_echo_text(address)
+        else:
+            echo_text = 'Не удалось распознать голосовое сообщение.'
     else:
-        send_message('Могу обработать только текстовое сообщение, голосовое сообщение и сообщение с координатами.',
-                     message_in)
+        echo_text = 'Могу обработать только текстовое сообщение, голосовое сообщение и сообщение с координатами.'
+    send_message(echo_text, message_in)
     return FUNC_RESPONSE
 
 
+def get_echo_text(address):
+    """Из строки адреса получает сообщение, которое отправится пользователю"""
+    lat, lon = get_coords_from_address(address)
+    if lat and lon:
+        echo_text = get_weather_info(lat, lon)
+    else:
+        echo_text = f'Недостаточно информации для адреса {address}.'
+    return echo_text
+
+
 def get_coords_from_address(address):
+    """Получает координаты (долготу и широту) из адреса."""
     r_address = requests.post(url="https://cleaner.dadata.ru/api/v1/clean/address",
                               headers={"Authorization": f"Token {DD_API_KEY}", "X-Secret": DD_SEC_KEY},
                               json=[address])
@@ -84,6 +93,7 @@ def get_coords_from_address(address):
 
 
 def get_weather_info(lat, lon):
+    """Выдает информацию о погоде по координатам."""
     r = requests.get(url="https://api.openweathermap.org/data/2.5/weather",
                      params={'lat': lat, "lon": lon, 'appid': OW_API_KEY, 'units': 'metric', "lang": 'ru'})
     info_weather = r.json()
